3c7098a4545da85a6f106a0a9b52aa204ed97d05
Jaime Alonso <jaimea111@gmail.com>
2024-03-27 05:11:56 -0500

    Adding Card component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import './index.css'
 import Layout from '../../Components/Layout';
+import Card from "../../Components/Card/index.jsx";
 function Home() {
 
     return (
         <Layout>
             HOME
+            <Card />
         </Layout>
     );
 }
