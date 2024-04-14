06b510da808d5aa74219dd4e4bb21f3ac36dfbd9
Jaime Alonso <jaimea111@gmail.com>
2024-04-14 03:39:27 -0500

    Adding open and close behavior to the product details window

--- src/Components/ProductDetails/index.jsx
+++ src/Components/ProductDetails/index.jsx
@@ -1,14 +1,20 @@
 import './index.css';
 import { XMarkIcon } from "@heroicons/react/24/solid/index.js";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ShoppingCartContext } from "../../Context/index.jsx";
 
 const ProductDetails = () => {
+    const context = useContext(ShoppingCartContext);
     const [hover, setHover] = useState(false);
     return (
-        <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
+        <aside
+            className={`${context.isProductDetailOpen? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Details</h2>
-                <div className='cursor-pointer' onMouseEnter={()=> setHover(true)} onMouseLeave={()=> setHover(false)}>
+                <div className='cursor-pointer'
+                     onMouseEnter={()=> setHover(true)}
+                     onMouseLeave={()=> setHover(false)} 
+                     onClick={() => context.closeProductDetail()}>
                     <XMarkIcon className={hover ? "h-6 w-6 text-red-700 bg-gray-300 rounded-md":"h-6 w-6 text-black-700"}/>
                 </div>
             </div>
